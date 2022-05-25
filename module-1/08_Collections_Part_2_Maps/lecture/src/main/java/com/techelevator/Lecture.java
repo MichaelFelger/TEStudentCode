cafe680472a0d5e8eb0f0bb1c857a05f07cba4d1
Michael Felger <michaelfelger@gmail.com>
2022-05-25 12:19:20 -0400

    updated lecture final

--- module-1/08_Collections_Part_2_Maps/lecture/src/main/java/com/techelevator/Lecture.java
+++ module-1/08_Collections_Part_2_Maps/lecture/src/main/java/com/techelevator/Lecture.java
@@ -3,6 +3,7 @@ package com.techelevator;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Set;
+import java.util.List;
 
 public class Lecture {
 
@@ -12,6 +13,42 @@ public class Lecture {
 		System.out.println("        MAPS");
 		System.out.println("####################");
 		System.out.println();
+
+		Map<String, Integer> myMap = new HashMap<>();
+		myMap.put("A", 100);
+		myMap.put("B", 90);
+		myMap.put("C", 80);
+
+		System.out.println("retrieving A "  + myMap.get("A"));
+		System.out.println("retrieving Z " + myMap.get("Z"));
+
+		System.out.println("does 'A' exist? " + myMap.containsKey("A"));
+
+		myMap.keySet();
+		// can iterate through keys if one needs to, but they won't be in order
+		System.out.println("**************");
+		for (String key : myMap.keySet()) {
+			System.out.println(key);
+		}
+
+		myMap.remove("C");
+		System.out.println(myMap);
+		for (String key : myMap.keySet()) {
+			System.out.println(key);
+		}
+
+		myMap.put("C", 60);
+		for (Map.Entry<String,Integer> entry : myMap.entrySet()) {
+			System.out.printf("Key: %s Value: s%\n", entry.getKey(), entry.getValue());
+		}
+
+		myMap.put("C", 70);
+
+	//	Map<String, List<Integer>> anotherList = new HashMap<>();
+		//              ^^^
+		// so you can get complex data structures by using other data types for the value
+		// you could do the same with the keys, but we aren't really going to here
+		// this is going to be a multi-step process to access the data
 
 
 	}
