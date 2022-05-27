1e7fc8b506941388c7ec7b3617a1b28c6fc1f6f2
Michael Felger <michaelfelger@gmail.com>
2022-05-27 18:51:13 -0400

    finished m1d9

--- module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
+++ module-1/09_Classes_and_Encapsulation/exercise/src/main/java/com/techelevator/Television.java
@@ -1,5 +1,70 @@
 package com.techelevator;
 
 public class Television {
+    private boolean isOn = false;
+    private int currentChannel = 3;
+    private int currentVolume = 2;
+
+    //getters
+    public boolean isOn() {
+        return isOn;
+    }
+
+    public int getCurrentChannel() {
+        return currentChannel;
+    }
+
+    public int getCurrentVolume() {
+        return currentVolume;
+    }
+
+    //methods
+    public void turnOff() {
+        isOn = false;
+    }
+
+    public void turnOn() {
+        isOn = true;
+    }
+
+    public void changeChannel(int newChannel) {
+        if (isOn && newChannel > 2 && newChannel < 19) {
+            currentChannel = newChannel;
+        }
+    }
+
+    public void channelUp() {
+        if (isOn && currentChannel < 18) {
+            currentChannel += 1;
+        } else {
+            if (isOn && currentChannel == 18) {
+                currentChannel = 3;
+            }
+        }
+    }
+
+    public void channelDown() {
+        if (isOn && currentChannel > 3) {
+            currentChannel -= 1;
+        } else {
+            if (isOn && currentChannel == 3) {
+                currentChannel = 18;
+            }
+        }
+
+    }
+
+    public void raiseVolume() {
+        if (isOn && currentVolume < 10) {
+            currentVolume++;
+        }
+    }
+
+    public void lowerVolume() {
+        if (isOn && currentVolume > 0) {
+            currentVolume--;
+        }
+    }
+
 
 }
