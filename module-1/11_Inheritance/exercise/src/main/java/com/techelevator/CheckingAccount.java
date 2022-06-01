7de43746db5537bc078649f9d4a57680fbe14d62
Michael Felger <michaelfelger@gmail.com>
2022-06-01 14:55:35 -0400

    updated homework from yesterday

--- module-1/11_Inheritance/exercise/src/main/java/com/techelevator/CheckingAccount.java
+++ module-1/11_Inheritance/exercise/src/main/java/com/techelevator/CheckingAccount.java
@@ -3,7 +3,7 @@ package com.techelevator;
 public class CheckingAccount extends BankAccount {
 
     public static int overdraftFee = 10;
-    public int theBalance = getBalance();
+//    public int theBalance = getBalance();
 
     //constructors
     public CheckingAccount(String accountHolderName, String accountNumber) {
@@ -15,12 +15,12 @@ public class CheckingAccount extends BankAccount {
 
     @Override
     public int withdraw (int amountToWithdraw) {
-        if ((theBalance - amountToWithdraw) > 0) {
+        if ((getBalance() - amountToWithdraw) > 0) {
             return super.withdraw(amountToWithdraw);
-        } else if (theBalance - amountToWithdraw > -100) {
+        } else if (getBalance() - amountToWithdraw > -100) {
             return super.withdraw(amountToWithdraw + overdraftFee);
         } else {
-            return theBalance;
+            return getBalance();
         }
     }
 
