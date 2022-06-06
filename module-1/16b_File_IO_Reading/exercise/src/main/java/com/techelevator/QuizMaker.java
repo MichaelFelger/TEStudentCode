6689d49707d17bbbe9e092b9c2a3afc4e1df9e3b
Michael Felger <michaelfelger@gmail.com>
2022-06-06 15:44:56 -0400

    some io homework progress

--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/QuizMaker.java
@@ -1,10 +1,27 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
 public class QuizMaker {
 
-	public static void main(String[] args) {
-		
+    public static void main(String[] args) throws FileNotFoundException {
 
-	}
+        Scanner userInput = new Scanner(System.in);
+        System.out.print("What is the fully qualified name of the file that should be searched? ");
+        String filePath = userInput.nextLine();
+        File questionsFile = new File(filePath);
+
+
+        Scanner fileInput = new Scanner(questionsFile);
+        int totalNumberOfQuestions = 0;
+        while (fileInput.hasNextLine()) {
+            totalNumberOfQuestions++;
+        }
+        
+
+
+    }
 
 }
--- module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
+++ module-1/16b_File_IO_Reading/exercise/src/main/java/com/techelevator/WordSearch.java
@@ -1,9 +1,74 @@
 package com.techelevator;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Locale;
+import java.util.Objects;
+import java.util.Scanner;
+
 public class WordSearch {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
 
-	}
+        try {
+            Scanner userInput = new Scanner(System.in);
+            System.out.print("What is the fully qualified name of the file that should be searched? ");
+            String filePath = userInput.nextLine();
 
+            System.out.print("What word shall we search for? ");
+            String searchWord = userInput.nextLine();
+            File bookFile = new File(filePath);
+
+
+            System.out.println("Should this search be case sensitive? (Y/N): ");
+            boolean caseSensitive = userInput.nextLine().equalsIgnoreCase("Y");
+            userInput.close();
+
+
+            Scanner fileInput = new Scanner(bookFile);
+            int lineCount = 0;
+
+            while (fileInput.hasNextLine()) {
+                lineCount++;
+                String lineOfText = fileInput.nextLine();
+                if (caseSensitive) {
+                    if (lineOfText.contains(searchWord)) {
+                        System.out.println(lineCount + ": " + lineOfText);
+                    }
+                } else {
+                    searchWord = searchWord.toLowerCase();
+                    String caseInsensitveLineOfText = lineOfText.toLowerCase();
+                    if (caseInsensitveLineOfText.contains(searchWord)) {
+                        System.out.println(lineCount + ": " + lineOfText);
+
+                    }
+                }
+            }
+
+//            if (caseSensitive) {
+//                while (fileInput.hasNextLine()) {
+//                    lineCount++;
+//                    String lineOfText = fileInput.nextLine();
+//                    if (lineOfText.contains(searchWord)) {
+//                        System.out.println(lineCount + ": " + lineOfText);
+//                    }
+//                }
+//            } else {
+//                while (fileInput.hasNextLine()) {
+//                    searchWord = searchWord.toLowerCase();
+//                    lineCount++;
+//                    String lineOfText = fileInput.nextLine();
+//                    String caseInsensitveLineOfText = lineOfText.toLowerCase();
+//                    if (caseInsensitveLineOfText.contains(searchWord)) {
+//                        System.out.println(lineCount + ": " + lineOfText);
+//                    }
+//                }
+//            }
+
+
+
+        } catch (FileNotFoundException fnf) {
+            System.out.println("file not found");
+        }
+    }
 }
