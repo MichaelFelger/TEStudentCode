a9fef0fbc6af5b0aed2f40f52d2404fc358dffc4
Michael Felger <michaelfelger@gmail.com>
2022-07-18 15:25:51 -0400

    m3d6 exercise no challenge yet

--- module-3/06_Intro_to_JavaScript/exercise/exercises.js
+++ module-3/06_Intro_to_JavaScript/exercise/exercises.js
@@ -1,80 +1,128 @@
 ﻿/*
-1. **sumDouble** Given two int values, return their sum. Unless the two values are the 
-    same, then return double their sum.
+1. **sumDouble** Given two int values, return their sum. Unless the two values are the
+	same, then return double their sum.
 
 		sumDouble(1, 2) → 3
 		sumDouble(3, 2) → 5
 		sumDouble(2, 2) → 8
 
 		function sumDouble(x, y) {
-			// do logic here
-			// return result;
-			return x + y;
-        }
+			let sum = x + y;
+			if (x==y) {
+				sum *=2;
+			}
+			return sum;
+		}
 */
 
+function sumDouble(x, y) {
+	let sum = x + y;
+	if (x == y) {
+		sum *= 2;
+	}
+	return sum;
+}
 /*
-2. **hasTeen** We'll say that a number is "teen" if it is in the range 13..19 inclusive. 
-    Given 3 int values, return true if 1 or more of them are teen.
 
-		hasTeen(13, 20, 10) → true
-		hasTeen(20, 19, 10) → true
-		hasTeen(20, 10, 13) → true
+2. **hasTeen** We'll say that a number is "teen" if it is in the range 13..19 inclusive.
+Given 3 int values, return true if 1 or more of them are teen.
+
+hasTeen(13, 20, 10) → true
+hasTeen(20, 19, 10) → true
+hasTeen(20, 10, 13) → true
 */
-
-/* 
-3. **lastDigit** Given two non-negative int values, return true if they have the same 
-    last digit, such as with 27 and 57.
+function hasTeen(x, y, z) {
+	let teen = { from: 13, to: 19 }
+	if ((x >= 13 && x <= 19) || (z >= 13 && z <= 19) || (y >= 13 && y <= 19)) {
+		return true;
+	} else { return false; }
+}
+/*
+3. **lastDigit** Given two non-negative int values, return true if they have the same
+	last digit, such as with 27 and 57.
 
 		lastDigit(7, 17) → true
 		lastDigit(6, 17) → false
 		lastDigit(3, 113) → true
 */
-
+function lastDigit(x, y) {
+	const lastX = String(x).slice(-1);
+	const lastY = String(y).slice(-1);
+	if (lastX === lastY) {
+		return true;
+	} else { return false; }
+}
 /*
-4. **seeColor** Given a string, if the string begins with "red" or "blue" return that color 
-    string, otherwise return the empty string.
+4. **seeColor** Given a string, if the string begins with "red" or "blue" return that color
+	string, otherwise return the empty string.
 
 		seeColor("redxx") → "red"
 		seeColor("xxred") → ""
-        seeColor("blueTimes") → "blue"
+		seeColor("blueTimes") → "blue"
 */
-
+function seeColor(myString) {
+	if (myString.substring(0, 3) == 'red') {
+		myString = 'red';
+		return myString;
+	} else if (myString.substring(0, 4) == 'blue') {
+		myString = 'blue';
+		return myString;
+	} else myString = '';
+	return myString;
+}
 /*
 5. **oddOnly** Write a function that given an array of integer of any length, removes
-    the even numbers, and returns a new array of just the the odd numbers.
+	the even numbers, and returns a new array of just the the odd numbers.
 
 		oddOnly([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11]) → [1, 3, 5, 7, 9, 11];
 		oddOnly([2, 4, 8, 32, 256]); → []
 */
-
+function oddOnly(myArray) {
+	newArray = [];
+	for (let i = 0; i < myArray.length; i++) {
+		if (myArray[i] % 2 == 1) {
+			newArray.push(myArray[i]);
+		}
+	}
+	return newArray;
+}
 /*
-6. **frontAgain** Given a string, return true if the first 2 chars in the string also appear 
-    at the end of the string, such as with "edited".
+6. **frontAgain** Given a string, return true if the first 2 chars in the string also appear
+	at the end of the string, such as with "edited".
 
 		frontAgain("edited") → true
 		frontAgain("edit") → false
 		frontAgain("ed") → true
 */
-
+function frontAgain(someString) {
+	front = someString.substring(0, 2);
+	end = someString.substring(someString.length - 2)
+	if (front == end) {
+		return true;
+	} else { return false; }
+}
 /*
-7. **cigarParty** When squirrels get together for a party, they like to have cigars. 
-A squirrel party is successful when the number of cigars is between 40 and 60, inclusive. 
-Unless it is the weekend, in which case there is no upper bound on the number of cigars. 
-Write a squirrel party function that return true if the party with the given values is successful, 
+7. **cigarParty** When squirrels get together for a party, they like to have cigars.
+A squirrel party is successful when the number of cigars is between 40 and 60, inclusive.
+Unless it is the weekend, in which case there is no upper bound on the number of cigars.
+Write a squirrel party function that return true if the party with the given values is successful,
 or false otherwise.
 
 		cigarParty(30, false) → false
 		cigarParty(50, false) → true
 		cigarParty(70, true) → true
 */
-
+function cigarParty(numCigars, isWeekend) {
+	if ((!isWeekend && (numCigars >= 40 && numCigars <= 60)) || (isWeekend && (numCigars >= 40))) {
+		return true;
+	} else { return false; }
+}
 /*
 8. **fizzBuzz** Given a number, return a value according to the following rules:
 If the number is multiple of 3, return "Fizz."
-If the number is a multiple of 5, return "Buzz." 
+If the number is a multiple of 5, return "Buzz."
 If the number is a multiple of both 3 and 5, return "FizzBuzz."
-In all other cases return the original number. 
+In all other cases return the original number.
 
 	fizzBuzz(3) → "Fizz"
 	fizzBuzz(1) → 1
@@ -82,7 +130,12 @@ In all other cases return the original number.
 	fizzBuzz(15) → "FizzBuzz"
 	fizzBuzz(8) → 8
 */
-
+function fizzBuzz(myNum) {
+	if ((myNum % 3 == 0) && (myNum % 5 == 0)) { return 'FizzBuzz' }
+	else if (myNum % 3 == 0) { return 'Fizz' }
+	else if (myNum % 5 == 0) { return 'Buzz' }
+	else return myNum;
+}
 /*
 9. **filterEvens** Write a function that filters an array to only include even numbers.
 
@@ -91,7 +144,14 @@ In all other cases return the original number.
 	filterEvens([2, 4, 6]) → [2, 4, 6]
 	filterEvens([100, 8, 21, 24, 62, 9, 7]) → [100, 8, 24, 62]
 */
-
+function filterEvens(numbers) {
+	let result = [];
+	for (const number of numbers) {
+		if (number % 2 == 0) {
+			result.push(number);
+		}
+	} return result;
+}
 /*
 10. **filterBigNumbers** Write a function that filters numbers greater than or equal to 100.
 
@@ -99,17 +159,32 @@ In all other cases return the original number.
 	filterBigNumbers([3, 2, 7, 1, -100, -120]) → []
 	filterBigNumbers([]) → []
 */
+function filterBigNumbers(numbers) {
+	let result = [];
+	for (const number of numbers) {
+		if (number >= 100) {
+			result.push(number);
+		}
+	} return result;
+}
 
 /*
-11. **filterMultiplesOfX** Write a function to filter numbers that are a multiple of a 
+11. **filterMultiplesOfX** Write a function to filter numbers that are a multiple of a
 parameter, `x` passed in.
 
 	filterMultiplesOfX([3, 5, 1, 9, 18, 21, 42, 67], 3) → [3, 9, 18, 21, 42]
 	filterMultiplesOfX([3, 5, 10, 20, 18, 21, 42, 67], 5) → [5, 10, 20]
 */
-
+function filterMultiplesOfX(numbers, x) {
+	let result = [];
+	for (const number of numbers) {
+		if (number % x == 0) {
+			result.push(number);
+		}
+	} return result;
+}
 /*
-12. **createObject** Write a function that creates an object with a property called 
+12. **createObject** Write a function that creates an object with a property called
 firstName, lastName, and age. Populate the properties with your values.
 
 	createObject() →
@@ -120,3 +195,12 @@ firstName, lastName, and age. Populate the properties with your values.
 		age
 	}
 */
+
+function createObject() {
+	const person = {
+		firstName: 'Michael',
+		lastName: 'Felger',
+		age: 40
+	}
+	return person;
+}
--- module-3/06_Intro_to_JavaScript/lecture/script.js
+++ module-3/06_Intro_to_JavaScript/lecture/script.js
@@ -2,3 +2,42 @@
 /**
  * Into to JavaScript
  */
+
+function variables() {
+
+}
+// assumed that all functions return something: could be at least 'undefined'
+
+// errors are going to happen when you run it, not when you write it - no compiler the help you here
+// LINTER tooling can help, but won't 
+
+// have a look at lecture final for some good examples
+
+// browser dev tools has a console where you can run and look at all of this stuff running
+
+function math() {
+    console.log("2" + 2);
+}
+
+// object literals are kind of like key value pairs on a map
+function hasObject() {
+    const person = {
+        firstName: "Michael",
+        lastName: "Felger",
+        pets: ["Sophie", "Billie"]
+    }
+    console.log(person)
+}
+
+// method overloading does not exist in JS - the last definition encountered is the ONLY definition of a function
+function add(x, y) {
+    return x + y;
+}
+
+function add(x, y, z) {
+    return x + y + z;
+}
+
+// many build in libraries - math library, string functions
+
+// it's all on MDN, so just look it up
