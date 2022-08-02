4faf922d37f1f386fa5b6393b19e75046098ec77
Michael Felger <michaelfelger@gmail.com>
2022-08-02 08:23:43 -0400

    finished exercise

--- module-3/13_Vue_Component_Communication/exercise/src/components/NewBookForm.vue
+++ module-3/13_Vue_Component_Communication/exercise/src/components/NewBookForm.vue
@@ -1,38 +1,28 @@
 <template>
   <div>
-    <a
-      id="show-form-button"
-      href="#"
-      v-if="showForm === false"
-      v-on:click.prevent="showForm = true"
-      >Add a book to your reading list</a
-    >
-
-    <form v-on:submit.prevent="addNewReview" v-if="showForm === true">
-      <div class="form-element">
-        <label for="reviewer">Name:</label>
-        <input id="reviewer" type="text" v-model="newReview.reviewer" />
-      </div>
-      <div class="form-element">
-        <label for="title">Title:</label>
-        <input id="title" type="text" v-model="newReview.title" />
-      </div>
-      <div class="form-element">
-        <label for="rating">Rating:</label>
-        <select id="rating" v-model.number="newReview.rating">
-          <option value="1">1 Star</option>
-          <option value="2">2 Stars</option>
-          <option value="3">3 Stars</option>
-          <option value="4">4 Stars</option>
-          <option value="5">5 Stars</option>
-        </select>
-      </div>
-      <div class="form-element">
-        <label for="review">Review:</label>
-        <textarea id="review" v-model="newReview.review"></textarea>
-      </div>
-      <input type="submit" value="Save" />
-      <input type="button" value="Cancel" v-on:click.prevent="resetForm" />
+    <form class="new-book-form">
+      <label for="title">Title:</label>
+      <input
+        id="title"
+        class="title-input"
+        type="text"
+        v-model="newBook.title"
+      />
+      <label for="author">Author:</label>
+      <input
+        id="author"
+        class="author-input"
+        type="text"
+        v-model="newBook.author"
+      />
+      <label for="isbn">ISBN:</label>
+      <input
+        id="isbn"
+        class="isbn-input"
+        type="number"
+        v-model="newBook.isbn"
+      />
+      <button type="submit" @click.prevent="addNewBook">Save Book</button>
     </form>
   </div>
 </template>
@@ -42,20 +32,16 @@ export default {
   name: "new-book-form",
   data() {
     return {
-      showForm: false,
-      newReview: {
-        favorited: false,
-      },
+      newBook: {},
     };
   },
   methods: {
-    addNewReview() {
-      this.$store.commit("ADD_REVIEW", this.newReview);
+    addNewBook() {
+      this.$store.commit("ADD_BOOK", this.newBook);
       this.resetForm();
     },
     resetForm() {
-      this.showForm = false;
-      this.newReview = {};
+      this.newBook = {};
     },
   },
 };
